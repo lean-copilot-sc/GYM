9abf6ad45516f1cbc66c389f152e80ef7536d0ef
Gaurang <Gaurang-at-329474049047>
2025-12-02 15:19:28 +0530

    Commit

--- gym/src/features/ui/selectors.ts
+++
@@ -1,4 +0,0 @@
-import type { RootState } from '@/store/store';
-
-export const selectCurrentTheme = (state: RootState) => state.ui.themeMode;
-export const selectSnackbar = (state: RootState) => state.ui.snackbar;
